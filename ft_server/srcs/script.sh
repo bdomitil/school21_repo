31901b2e8346d76befd68563919c9e613de4d8fe
Bracken Domitila <bdomitil@et-c4.kzn.21-school.ru>
2021-01-30 18:08:53 +0300

    passed version

--- ft_server/srcs/script.sh
+++ ft_server/srcs/script.sh
@@ -10,6 +10,7 @@ cat default > /etc/nginx/sites-enabled/default
 mv site1 site2 phpMyAdmin /var/www/html/;
 ln -s /var/www/html/phpMyAdmin /var/www/html/phpmyadmin;
 mv /var/www/html/index.nginx-debian.html  /var/www/html/index.html
+mv response_page/ /var/www/html/
 mv wordpress /var/www/wordpress
 chown -R www-data:www-data /var/www/wordpress
 service mysql start
@@ -18,7 +19,6 @@ echo "CREATE DATABASE wordpress;" | mysql -u root --skip-password
 echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost';" | mysql -u root --skip-password
 echo "FLUSH PRIVILEGES;" | mysql --skip-password
 openssl req -x509 -nodes -days 365 -subj "/C=KR/ST=Korea/L=Seoul/O=innoaca/OU=42seoul/CN=forhjy" -newkey rsa:2048 -keyout /etc/ssl/nginx-selfsigned.key -out /etc/ssl/nginx-selfsigned.crt
-curl -s https://api.wordpress.org/secret-key/1.1/salt/ > /var/www/wordpress/define_keys.txt
 service php7.3-fpm start
 service nginx start 
 bash
